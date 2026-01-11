d1ce4d68483f8177015f960a14042b60ed604c83
irfanwk <irfan.wk.2705@gmail.com>
2026-01-11 17:08:04 +0700

    12.6 add laravel breeze

--- w12/THT12/routes/web.php
+++ w12/THT12/routes/web.php
@@ -1,31 +1,32 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\TicketController;
 use App\Http\Controllers\CommentController;
+use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('login');
 });
 
-Route::middleware(['auth'])->group(function () {
-    // Halaman daftar tiket
+Route::get('/dashboard', function () {
+    return redirect()->route('tickets.index');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    // Ticket Routes
     Route::get('/tickets', [TicketController::class, 'index'])->name('tickets.index');
-    
-    // Halaman form tambah tiket
     Route::get('/tickets/create', [TicketController::class, 'create'])->name('tickets.create');
-    
-    // Proses simpan tiket ke database
     Route::post('/tickets', [TicketController::class, 'store'])->name('tickets.store');
-    
-    // Detail tiket (untuk diskusi)
     Route::get('/tickets/{ticket}', [TicketController::class, 'show'])->name('tickets.show');
-
-    // Route untuk simpan komentar
-    Route::post('/tickets/{ticket}/comments', [CommentController::class, 'store'])->name('comments.store');
-
-    // Route khusus Admin untuk update status tiket
     Route::patch('/tickets/{ticket}/status', [TicketController::class, 'updateStatus'])->name('tickets.updateStatus');
+
+    // Comment Routes
+    Route::post('/tickets/{ticket}/comments', [CommentController::class, 'store'])->name('comments.store');
 });
 
 require __DIR__.'/auth.php';
